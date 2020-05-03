e436fe9befa6683f6cefd1a7dc53ee421e55fb55
Glitch (typescript-fullstack) <none>
2020-05-03 21:37:51 +0000

    🍼👳 Checkpoint ./src/views/index.html:7701387/267 ./src/public/client.ts:7701387/2215 ./package.json:7701387/27

--- src/public/client.ts
+++ src/public/client.ts
@@ -1,13 +1,35 @@
-import { ACoolClass } from "./ACoolClass";
+import * as PIXI from 'pixi.js'
 
 console.log("Hello from client side TypeScript");
 
-const instance = new ACoolClass("hi");
 
-const logToConsole = (ourClass: ACoolClass) => {
-  console.log(ourClass.Param);
+var stage = new PIXI.Application({ width: 640, height: 360 });
+document.body.appendChild(stage.view);
+
+const renderer = PIXI.autoDetectRenderer(512, 384,  { view:document.getElementById("game-canvas") });
+
+const farTexture = PIXI.Texture.fromImage("resources/bg-far.png");	
+const far = new PIXI.extras.TilingSprite(farTexture, 512, 256);
+far.position.x = 0;
+far.position.y = 0;
+far.tilePosition.x = 0;
+far.tilePosition.y = 0;
+stage.addChild(far);
+
+const midTexture = PIXI.Texture.fromImage("resources/bg-mid.png");
+const mid = new PIXI.extras.TilingSprite(midTexture, 512, 256);
+mid.position.x = 0;
+mid.position.y = 128;
+mid.tilePosition.x = 0;
+mid.tilePosition.y = 0;
+stage.addChild(mid);
+
+function update() {
+  far.tilePosition.x -= 0.128;
+  mid.tilePosition.x -= 0.64;
+
+  renderer.render(stage);
+  requestAnimationFrame(update);
 }
 
-
-logToConsole(instance); // Works fine
-// logToConsole("something else"); // Fails at compile time!
+requestAnimationFrame(update);
