822816f30af79bd42cc8b5bb955776c215544f83
Glitch (typescript-fullstack) <none>
2020-05-03 21:49:43 +0000

    😬👩‍👩‍👦 Checkpoint ./src/public/client.ts:7701387/1485 ./.glitch-assets:7701387/746

--- src/public/client.ts
+++ src/public/client.ts
@@ -3,32 +3,39 @@ import * as PIXI from 'pixi.js'
 console.log("Hello from client side TypeScript");
 
 
-var stage = new PIXI.Application({ width: 640, height: 360 });
-document.body.appendChild(stage.view);
+var app = new PIXI.Application({ width: 640, height: 360 });
+document.body.appendChild(app.view);
 
-const renderer = PIXI.autoDetectRenderer(512, 384,  { view:document.getElementById("game-canvas") });
+var circle = new PIXI.Graphics();
+circle.beginFill(0x5cafe2);
+circle.drawCircle(0, 0, 80);
+circle.x = 320;
+circle.y = 180;
+//app.stage.addChild(circle);
 
-const farTexture = PIXI.Texture.fromImage("resources/bg-far.png");	
-const far = new PIXI.extras.TilingSprite(farTexture, 512, 256);
+
+const farTexture = PIXI.Texture.from("https://cdn.glitch.com/80146c7e-c8f9-483d-a85c-39f2fc095273%2Fbg-far.png?v=1588542214676");	
+const far = new PIXI.TilingSprite(farTexture, 512, 256);
 far.position.x = 0;
 far.position.y = 0;
 far.tilePosition.x = 0;
 far.tilePosition.y = 0;
-stage.addChild(far);
+app.stage.addChild(far);
 
-const midTexture = PIXI.Texture.fromImage("resources/bg-mid.png");
-const mid = new PIXI.extras.TilingSprite(midTexture, 512, 256);
+const midTexture = PIXI.Texture.from("https://cdn.glitch.com/80146c7e-c8f9-483d-a85c-39f2fc095273%2Fbg-mid.png?v=1588542216156");
+const mid = new PIXI.TilingSprite(midTexture, 512, 256);
 mid.position.x = 0;
 mid.position.y = 128;
 mid.tilePosition.x = 0;
 mid.tilePosition.y = 0;
-stage.addChild(mid);
+app.stage.addChild(mid);
+
 
 function update() {
   far.tilePosition.x -= 0.128;
   mid.tilePosition.x -= 0.64;
 
-  renderer.render(stage);
+  app.renderer.render(app.stage);
   requestAnimationFrame(update);
 }
 
